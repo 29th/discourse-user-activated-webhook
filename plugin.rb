7e48e41c208ee20d47259578b7cfcb7d33bee5d3
wilson29thid <wilson@29th.org>
2021-03-30 06:51:31 +0100

    Ensure user is activate This avoids an edge case that probably does not exist

--- plugin.rb
+++ plugin.rb
@@ -8,7 +8,7 @@ enabled_site_setting :discourse_user_activated_webhook_enabled
 
 after_initialize do
   add_model_callback(User, :after_update) do
-    if saved_change_to_active && email_confirmed?
+    if saved_change_to_active && active == true && email_confirmed?
       WebHook.enqueue_object_hooks(:user, self, "user_activated")
     end
   end
